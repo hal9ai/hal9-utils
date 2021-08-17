8e285ff57e405b6fd672b74c99479fa384e7c48f
Javier Luraschi <jluraschi@gmail.com>
2021-08-17 00:29:17 -0400

    port utils from hal9ai repo

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const package = require('./package.json');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'hal9-utils': './index.js',
+    'hal9-utils.min': './index.js',
+  },
+  devtool: "source-map",
+  output: {
+    filename: '[name].js',
+    library: {
+      name: 'hal9-utils',
+      type: 'umd2',
+      export: 'default',
+    },
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'none',
+  plugins: [
+  ],
+  module: {
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
+};
