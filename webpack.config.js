86b13ea591b20615161b9b537938417dee92aa17
Javier Luraschi <jluraschi@gmail.com>
2021-08-17 17:20:03 -0400

    rename library to make it easy to access

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     library: {
-      name: 'hal9-utils',
+      name: 'hal9.utils',
       type: 'umd2',
       export: 'default',
     },
